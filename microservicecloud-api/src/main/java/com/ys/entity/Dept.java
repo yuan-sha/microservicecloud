9380d7b37fc09a64fa6616bcf6c5e468aaa987b0
185388 <ysha@progrexion.com>
2021-02-23 14:01:49 -0700

    add microservicecloud-api

---
+++ microservicecloud-api/src/main/java/com/ys/entity/Dept.java
@@ -0,0 +1,18 @@
+package com.ys.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private long deptno;
+    private String dname;
+    private String db_source;
+}
