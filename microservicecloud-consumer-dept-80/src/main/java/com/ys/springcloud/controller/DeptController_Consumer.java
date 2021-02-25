1fe5baa23feec8abc0957227026eaf29cca8f21e
185388 <ysha@progrexion.com>
2021-02-25 16:01:21 -0700

    Server discovery

--- microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/controller/DeptController_Consumer.java
@@ -31,4 +31,9 @@ public class DeptController_Consumer {
     public List<Dept> list(){
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
     }
+
+    @RequestMapping(value = "/consumer/dept/discovery")
+    public Object discovery(){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/discovery",Object.class);
+    }
 }
--- microservicecloud-provider-dept-8001/src/main/java/com/ys/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/ys/controller/DeptController.java
@@ -1,8 +1,11 @@
 package com.ys.controller;
 
+
 import com.ys.entity.Dept;
 import com.ys.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +14,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add", method= RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -25,5 +31,17 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method= RequestMethod.GET)
     public List<Dept> list() {
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method= RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("*********"+list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element:srvList) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
