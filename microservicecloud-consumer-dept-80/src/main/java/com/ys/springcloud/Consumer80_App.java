b0391c14ba241df3b7188a84c1c5e1083046f648
185388 <ysha@progrexion.com>
2021-02-28 16:21:27 -0700

    Ribbon Client

--- microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/Consumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/Consumer80_App.java
@@ -2,8 +2,10 @@ package com.ys.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Consumer80_App {
     public static void main(String[] args) {
         SpringApplication.run(Consumer80_App.class,args);
