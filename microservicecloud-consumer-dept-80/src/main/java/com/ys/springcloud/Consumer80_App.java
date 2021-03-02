735be0102e57075a42c780467ff90a258c0dc533
185388 <ysha@progrexion.com>
2021-03-02 16:05:15 -0700

    config rule

--- microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/Consumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ys/springcloud/Consumer80_App.java
@@ -1,11 +1,14 @@
 package com.ys.springcloud;
 
+import com.ys.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="MICROSERVICECLOUD-DEPT", configuration = MyselfRule.class)
 public class Consumer80_App {
     public static void main(String[] args) {
         SpringApplication.run(Consumer80_App.class,args);
