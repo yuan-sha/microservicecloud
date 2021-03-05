3aa33f9f1a1d86ab93f9f6f2aab9fec2c6050e14
185388 <ysha@progrexion.com>
2021-03-04 21:54:19 -0700

    Add Hystrix downgrade

--- microservicecloud-consumer-dept-feign-80/src/main/java/com.ys/springcloud/Consumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign-80/src/main/java/com.ys/springcloud/Consumer80_Feign_App.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.ys.service","com.ys"})
+@SpringBootApplication
 @EnableEurekaClient
 @RibbonClient(name="MICROSERVICECLOUD-DEPT", configuration = MyselfRule.class)
-@EnableFeignClients("com.ys.service")
+@EnableFeignClients(basePackages = {"com.ys.service"})
+@ComponentScan("com.ys")
 public class Consumer80_Feign_App {
     public static void main(String[] args) {
         SpringApplication.run(Consumer80_Feign_App.class,args);
