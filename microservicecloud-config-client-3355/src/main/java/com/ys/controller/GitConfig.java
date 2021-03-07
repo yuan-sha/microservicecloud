84e303fb4264b8ecb160a93b4150adf7b89cc483
185388 <ysha@progrexion.com>
2021-03-06 20:47:32 -0700

    Add spring-config-client

---
+++ microservicecloud-config-client-3355/src/main/java/com/ys/controller/GitConfig.java
@@ -0,0 +1,18 @@
+package com.ys.controller;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class GitConfig {
+    @Value("${data.env}")
+    private String env;
+
+    @Value("${data.user.username}")
+    private String username;
+
+    @Value("${data.user.password}")
+    private String password;
+}
---
+++ microservicecloud-config-client-3355/src/main/java/com/ys/controller/GitController.java
@@ -0,0 +1,25 @@
+package com.ys.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+
+    @Autowired
+    private GitConfig gitConfig;
+
+    @Autowired
+    private GitAutoRefreshConfig gitAutoRefreshConfig;
+
+    @GetMapping(value = "show")
+    public Object show(){
+        return gitConfig;
+    }
+
+    @GetMapping(value = "autoShow")
+    public Object autoShow(){
+        return gitAutoRefreshConfig;
+    }
+}
